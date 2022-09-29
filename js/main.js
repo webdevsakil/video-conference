f91cde0ade30563e7c389e74285d245efdb33265
Sakil Anwar <sakilanwar60@gmail.com>
2022-09-29 07:10:40 +0600

    testimonial slider

--- js/main.js
+++ js/main.js
@@ -82,30 +82,25 @@
             asNavFor: '.slider-for',
             dots: true,
             focusOnSelect: true,
+            infinite: true,
             responsive: [
                 {
-                    breakpoint: 768,
+                    breakpoint: 1024,
                     settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
                         infinite: true,
                         dots: true
                     }
                 },
+               
                 {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 2,
-                    }
-                },
-                {
-                    breakpoint: 480,
+                    breakpoint: 768,
                     settings: {
                         slidesToShow: 1,
-                   
+                        slidesToScroll: 1
                     }
                 }
-
             ]
         });
 
